ad2f548a8a6a700ded080c00fbb181aa514729c3
Richard <richardebboh@gmail.com>
2021-12-18 16:19:53 -0800

    Implemented greeting user with their name on login

--- app/src/main/java/com/solid/todolistapp/view/fragments/HomeFragment.kt
+++ app/src/main/java/com/solid/todolistapp/view/fragments/HomeFragment.kt
@@ -1,6 +1,8 @@
 package com.solid.todolistapp.view.fragments
 
+import android.content.Context
 import android.os.Bundle
+import android.text.TextUtils
 import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
@@ -14,7 +16,6 @@ import com.solid.todolistapp.view.activities.todo.adapter.TodoAdapter
 class HomeFragment : Fragment(R.layout.fragment_home) {
 
     private lateinit var todoViewModel: TodoViewModel
-    private lateinit var userViewModel: UserViewModel
 
     private var _binding: FragmentHomeBinding? = null
     private val binding get() = _binding!!
@@ -31,6 +32,10 @@ class HomeFragment : Fragment(R.layout.fragment_home) {
             todoAdapter.setData(todo)
         })
 
+        val sharedPref = requireActivity().getPreferences(Context.MODE_PRIVATE)
+        val fullName = sharedPref.getString("fullName", "")
+        val name = TextUtils.substring(fullName, 0, fullName!!.indexOf(" "))
+        binding.greetText.text = String.format("%s", "Hello, $name")
 
     }
 
